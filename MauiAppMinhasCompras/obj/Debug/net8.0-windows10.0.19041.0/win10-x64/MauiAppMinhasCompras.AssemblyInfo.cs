58543cbeedecb734351245135b515943b1c9cec8
EKT <elias.kento@gmail.com>
2025-08-25 11:11:45 -0300

    Arquivos atualizados para Agenda 04

--- MauiAppMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiAppMinhasCompras.AssemblyInfo.cs
+++ MauiAppMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiAppMinhasCompras.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiAppMinhasCompras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d713d5bded120a60da0ef30f90245f381aaa7100")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAppMinhasCompras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAppMinhasCompras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
